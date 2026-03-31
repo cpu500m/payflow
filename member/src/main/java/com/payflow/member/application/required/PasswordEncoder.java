739141a94e0acfc1e0fda48f08251acfaac23abf
cpu500m <paul108203@naver.com>
2026-03-31 23:35:40 +0900

    [feature_member] : 회원 부분 도메인 모델 문서 정리

--- member/src/main/java/com/payflow/member/application/required/PasswordEncoder.java
+++
@@ -1,8 +0,0 @@
-package com.payflow.member.application.required;
-
-public interface PasswordEncoder {
-
-    String encode(String rawPassword);
-
-    boolean matches(String rawPassword, String encodedPassword);
-}
